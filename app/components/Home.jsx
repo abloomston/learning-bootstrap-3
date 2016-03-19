1c3df217ec7d39ee35bef240accf97f6650b4151
Adam Bloomston <mail@adambloomston>
2016-03-19 13:24:02 -0400

    using react-bootstrap for carousel
    had problems with links, decided to try react-bootstrap

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,50 +1,45 @@
 var React = require('react');
 
+var Bootstrap = require('react-bootstrap');
+var Carousel = Bootstrap.Carousel;
+var CarouselItem = Bootstrap.CarouselItem;
+
 var Home = React.createClass({
 
 	render: function() {
 		return (
-      <div id="myCarousel" className="slide" ref={(ref) => ref && $(ref).carousel({interval: 4000})}>
-        <ol className="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="item active">
-            <img src={require("../images/slide1.jpg")}/>
-            <div className="container active">
-              <div className="carousel-caption">
-                <h1>MOVE ME</h1>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
-              </div>
+      <Carousel defaultActiveIndex={0} interval={4000}>
+        <CarouselItem>
+          <img src={require("../images/slide1.jpg")}/>
+          <div className="container active">
+            <div className="carousel-caption">
+              <h1>MOVE ME</h1>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+              <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
             </div>
           </div>
-          <div className="item">
-            <img src={require("../images/slide2.jpg")}/>
-            <div className="container active">
-              <div className="carousel-caption">
-                <h1>HEADING 2</h1>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.</p>
-                <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
-              </div>
+        </CarouselItem>
+        <CarouselItem>
+          <img src={require("../images/slide2.jpg")}/>
+          <div className="container active">
+            <div className="carousel-caption">
+              <h1>HEADING 2</h1>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.</p>
+              <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
             </div>
           </div>
-          <div className="item">
-            <img src={require("../images/slide3.jpg")}/>
-            <div className="container active">
-              <div className="carousel-caption">
-                <h1>HEADING 3</h1>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.</p>
-                <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
-              </div>
+        </CarouselItem>
+        <CarouselItem>
+          <img src={require("../images/slide3.jpg")}/>
+          <div className="container active">
+            <div className="carousel-caption">
+              <h1>HEADING 3</h1>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.</p>
+              <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
             </div>
           </div>
-        </div>
-        <a className="left carousel-control" href="#myCarousel" data-slide="prev"><span className="glyphicon glyphicon-chevron-left"></span></a>
-        <a className="right carousel-control" href="#myCarousel" data-slide="next"><span className="glyphicon glyphicon-chevron-right"></span></a>
-      </div>
+        </CarouselItem>
+      </Carousel>
 		);
 	}
 });
