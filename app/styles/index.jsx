25519c5060cd071be0bf14ef58b72bd6cf3cbcc9
Adam Bloomston <mail@adambloomston>
2016-03-19 12:34:19 -0400

    include css

--- app/styles/index.jsx
+++ app/styles/index.jsx
@@ -1,2 +1,4 @@
 window.jQuery = window.$ = require('jquery/dist/jquery.min');  // hack for providing jquery to boostrap, from http://stackoverflow.com/a/29450184/5327056
 require("bootstrap-webpack");
+
+require("./index.css");
