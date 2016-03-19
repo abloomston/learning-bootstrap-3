4a00fc5664c4e79e26980be63610f44cf867946c
Adam Bloomston <mail@adambloomston>
2016-03-19 12:21:02 -0400

    initial setup to require imgs
    * changed stateless functional components to regular components due to
      https://github.com/facebook/react/issues/6298

--- app/components/InfoBlock.jsx
+++ app/components/InfoBlock.jsx
@@ -1,45 +1,51 @@
 var React = require('react');
 
-var InfoBlock = props => (
-  <div className="info-block">
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6 tabbable">
-          <ul id="myTab" className="nav nav-tabs">
-            <li className="active"><a href="#android" data-toggle="tab">Android</a></li>
-            <li><a href="#ios" data-toggle="tab">iOS</a></li>
-            <li><a href="#win" data-toggle="tab">Windows</a></li>
-          </ul>
-          <div className="tab-content">
-            <div className="tab-pane fade in active" id="android">
-              <p>
-                <img src="images/gplay.jpg" alt="google" className="pull-right"/>
-                Android... Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus.<br/><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star-empty"></span>
-              </p>
+var React = require('react');
+
+var InfoBlock = React.createClass({
+	render: function() {
+		return (
+      <div className="info-block">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6 tabbable">
+              <ul id="myTab" className="nav nav-tabs">
+                <li className="active"><a href="#android" data-toggle="tab">Android</a></li>
+                <li><a href="#ios" data-toggle="tab">iOS</a></li>
+                <li><a href="#win" data-toggle="tab">Windows</a></li>
+              </ul>
+              <div className="tab-content">
+                <div className="tab-pane fade in active" id="android">
+                  <p>
+                    <img src={require("../images/gplay.jpg")} alt="google" className="pull-right"/>
+                    Android... Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus.<br/><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star-empty"></span>
+                  </p>
+                </div>
+                <div className="tab-pane fade in" id="ios">
+                  <p>
+                    <img src={require("../images/appstore.jpg")} alt="apple" className="pull-right"/>
+                    iOS... Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus.<br/><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star-empty"></span>
+                  </p>
+                </div>
+                <div className="tab-pane fade in" id="win">
+                  <p>
+                    <img src={require("../images/winstore.jpg")} alt="windows" className="pull-right"/>
+                    Windows... Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus.<br/><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star-empty"></span><span className="glyphicon glyphicon-star-empty"></span>
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="tab-pane fade in" id="ios">
-              <p>
-                <img src="images/appstore.jpg" alt="apple" className="pull-right"/>
-                iOS... Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus.<br/><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star-empty"></span>
-              </p>
-            </div>
-            <div className="tab-pane fade in" id="win">
-              <p>
-                <img src="images/winstore.jpg" alt="windows" className="pull-right"/>
-                Windows... Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus.<br/><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star-empty"></span><span className="glyphicon glyphicon-star-empty"></span>
-              </p>
+            <div className="col-sm-4 col-sm-offset-2">
+              <blockquote>
+                <p>&ldquo;This App is the best app I've ever used, ever. &rdquo;</p>
+                <small>James T. Kirk, <cite title="source title">Starship Enterprise</cite></small>
+              </blockquote>
             </div>
           </div>
         </div>
-        <div className="col-sm-4 col-sm-offset-2">
-          <blockquote>
-            <p>&ldquo;This App is the best app I've ever used, ever. &rdquo;</p>
-            <small>James T. Kirk, <cite title="source title">Starship Enterprise</cite></small>
-          </blockquote>
-        </div>
       </div>
-    </div>
-  </div>
-);
+		);
+	}
+});
 
 module.exports = InfoBlock;
--- app/components/MarketingBlock.jsx
+++ app/components/MarketingBlock.jsx
@@ -1,20 +1,24 @@
 var React = require('react');
 
-var MarketingBlock = props => (
-  <div className="marketing-block">
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6">
-          <img className="img-responsive" src="images/app.png"/>
-        </div>
-        <div className="col-sm-6">
-          <h3 className="tpad">MARKETING SLOGAN HERE</h3><hr/>
-          <p>Marketing Blah Blah Here. Blah Blah Blah.</p>
-          <p><a className="btn btn-lg btn-primary" href="#">Get it now!</a></p>
+var MarketingBlock = React.createClass({
+	render: function() {
+		return (
+      <div className="marketing-block">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <img className="img-responsive" src={require("../images/app.png")}/>
+            </div>
+            <div className="col-sm-6">
+              <h3 className="tpad">MARKETING SLOGAN HERE</h3><hr/>
+              <p>Marketing Blah Blah Here. Blah Blah Blah.</p>
+              <p><a className="btn btn-lg btn-primary" href="#">Get it now!</a></p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+		);
+	}
+});
 
 module.exports = MarketingBlock;
