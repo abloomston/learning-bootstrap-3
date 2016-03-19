80552ae802119695df0637f674469e952f9fe5f8
Adam Bloomston <mail@adambloomston>
2016-03-19 12:32:12 -0400

    minor cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,9 @@ module.exports = {
   ],
   module: {
     loaders: [
-      {test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader"},
-      {test: /\.css$/, loader: "style-loader!css-loader"},
-      { test: /\.(png|jpg)$/, loader: 'file-loader'},  // todo switch back to url-loader
+      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.(png|jpg)$/, loader: "file-loader" },  // todo switch back to url-loader
       { test: /\.(woff|woff2)$/,  loader: "url-loader?limit=10000&mimetype=application/font-woff" },
       { test: /\.ttf$/,    loader: "file-loader" },
       { test: /\.eot$/,    loader: "file-loader" },
