ba444e011ddde0a05d1f24ba1a6aca2923386f01
Adam Bloomston <mail@adambloomston>
2016-03-19 16:53:54 -0400

    using stateless functional components w/ =>

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,44 +4,39 @@ var Bootstrap = require('react-bootstrap');
 var Carousel = Bootstrap.Carousel;
 var CarouselItem = Bootstrap.CarouselItem;
 
-var Home = React.createClass({
-
-	render: function() {
-		return (
-      <Carousel defaultActiveIndex={0} interval={4000}>
-        <CarouselItem>
-          <img src={require("../images/slide1.jpg")}/>
-          <div className="container active">
-            <div className="carousel-caption">
-              <h1>MOVE ME</h1>
-              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-              <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
-            </div>
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <img src={require("../images/slide2.jpg")}/>
-          <div className="container active">
-            <div className="carousel-caption">
-              <h1>HEADING 2</h1>
-              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.</p>
-              <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
-            </div>
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <img src={require("../images/slide3.jpg")}/>
-          <div className="container active">
-            <div className="carousel-caption">
-              <h1>HEADING 3</h1>
-              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.</p>
-              <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
-            </div>
-          </div>
-        </CarouselItem>
-      </Carousel>
-		);
-	}
-});
+var Home = props => (
+  <Carousel defaultActiveIndex={0} interval={4000}>
+    <CarouselItem>
+      <img src={require("../images/slide1.jpg")}/>
+      <div className="container active">
+        <div className="carousel-caption">
+          <h1>MOVE ME</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+          <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
+        </div>
+      </div>
+    </CarouselItem>
+    <CarouselItem>
+      <img src={require("../images/slide2.jpg")}/>
+      <div className="container active">
+        <div className="carousel-caption">
+          <h1>HEADING 2</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.</p>
+          <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
+        </div>
+      </div>
+    </CarouselItem>
+    <CarouselItem>
+      <img src={require("../images/slide3.jpg")}/>
+      <div className="container active">
+        <div className="carousel-caption">
+          <h1>HEADING 3</h1>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.</p>
+          <p><a href="#" className="btn btn-primary btn-large">Sign up today</a></p>
+        </div>
+      </div>
+    </CarouselItem>
+  </Carousel>
+);
 
 module.exports = Home;
