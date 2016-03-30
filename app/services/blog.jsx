2a9570ac92ceaef7ef9ced92f0738c389ca4972a
Adam Bloomston <mail@adambloomston>
2016-03-30 10:34:24 -0400

    removed old TODO

--- app/services/blog.jsx
+++ app/services/blog.jsx
@@ -10,7 +10,7 @@ function simulateGetPosts(lastIndex, firstIndex) {
             author_link: "#",
             author_name: "Infinite Skills",
             date: moment().subtract(firstIndex + index, 'hours').format('MMMM Do YYYY, h:mm:ss a'),
-            image: require(`../images/blog${((lastIndex - index) % 3) + 1}.jpg`),/* TODO */
+            image: require(`../images/blog${((lastIndex - index) % 3) + 1}.jpg`),
             summary: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor. Nullam tristique diam non turpis. Cras placerat accumsan nulla. Nullam rutrum. Nam vestibulum accumsan nisl.",
             full_link: "#"
         }
