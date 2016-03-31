275940373d3877ece2c3e4851ee117cdccb000a3
Adam Bloomston <mail@adambloomston>
2016-03-31 15:53:22 -0400

    fix clicking link collapsing menu

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -2,28 +2,51 @@ import React from 'react';
 import {Nav, Navbar, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import {LinkContainer, IndexLinkContainer} from 'react-router-bootstrap';
 
-const NavBar = props => (
-  /* TODO when clicking a link, this does not collapse */
-  <Navbar inverse fixedTop fluid>
-    <Navbar.Header>
-      <IndexLinkContainer to="/"><Navbar.Brand>MoveMe</Navbar.Brand></IndexLinkContainer>
-      <Navbar.Toggle/>
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <IndexLinkContainer to="/"><NavItem>Home</NavItem></IndexLinkContainer>
-        <LinkContainer to="/blog"><NavItem>Blog</NavItem></LinkContainer>
-        <LinkContainer to="/clients"><NavItem>Clients</NavItem></LinkContainer>
-        <LinkContainer to="/contact"><NavItem>Contact</NavItem></LinkContainer>
-        <NavDropdown title="Social" id="social-links-dropdown">
-          <MenuItem href="https://www.facebook.com">Facebook</MenuItem>
-          <MenuItem href="https://www.twitter.com">Twitter</MenuItem>
-          <MenuItem href="https://plus.google.com">Google+</MenuItem>
-          <MenuItem href="https://www.linkedin.com">LinkedIn</MenuItem>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+const NavBar = React.createClass({
+
+  getInitialState: function() {
+    return {
+      expanded: false
+    }
+  },
+
+  toggleExpanded: function(expanded) {
+    this.setState({
+      expanded: expanded
+    });
+  },
+
+  onClickLink: function() {
+    this.toggleExpanded(false);
+  },
+
+	render: function() {
+		return (
+      <Navbar inverse fixedTop fluid
+              expanded={this.state.expanded}
+              onToggle={this.toggleExpanded}
+      >
+        <Navbar.Header>
+          <IndexLinkContainer to="/"><Navbar.Brand>MoveMe</Navbar.Brand></IndexLinkContainer>
+          <Navbar.Toggle/>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav onSelect={this.onClickLink}>
+            <IndexLinkContainer to="/"><NavItem>Home</NavItem></IndexLinkContainer>
+            <LinkContainer to="/blog"><NavItem>Blog</NavItem></LinkContainer>
+            <LinkContainer to="/clients"><NavItem>Clients</NavItem></LinkContainer>
+            <LinkContainer to="/contact"><NavItem>Contact</NavItem></LinkContainer>
+            <NavDropdown title="Social" id="social-links-dropdown">
+              <MenuItem href="https://www.facebook.com">Facebook</MenuItem>
+              <MenuItem href="https://www.twitter.com">Twitter</MenuItem>
+              <MenuItem href="https://plus.google.com">Google+</MenuItem>
+              <MenuItem href="https://www.linkedin.com">LinkedIn</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+		);
+	}
+});
 
 module.exports = NavBar;
